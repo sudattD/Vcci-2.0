bff6c5b51f778c50dbd3a3146c5b082f2b6f15b7
Sudatt <sudatt.d@servify.in>
2022-05-23 11:48:02 +0530

    minor changes

--- app/src/main/java/vcci/consumer/android/constants/RestClientConstants.java
+++ app/src/main/java/vcci/consumer/android/constants/RestClientConstants.java
@@ -3,9 +3,6 @@ package vcci.consumer.android.constants;
 public interface RestClientConstants {
 
 
-    String SBNRI_LOCAL = "https://dev.sbnri.com/api/";
-
-
     String VCCI_LOCAL = "https://www.niagidc.com/web-service/";
 
 
